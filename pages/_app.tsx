975491bcbe86305e0a6495b782b409219c030ad0
Hamidreza Ghanbari <hamidrezaghanbari13@gmail.com>
2023-12-13 12:49:50 +0330

    chore(installation & configuration): install and config project react query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/assets/globals.css";
+import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import {
+  HydrationBoundary,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { useState } from "react";
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <HydrationBoundary state={pageProps.dehydratedState}>
+          <Component {...pageProps} />
+      </HydrationBoundary>
+    </QueryClientProvider>
+  );
 }
